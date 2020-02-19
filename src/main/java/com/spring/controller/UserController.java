6e2ac84301104a35057a6f3fb1b0a1740e2424ec
Taras48 <red.shuhov48@gmail.com>
2020-02-19 19:20:13 +0300

    исправлено обновление

--- src/main/java/com/spring/controller/UserController.java
+++ src/main/java/com/spring/controller/UserController.java
@@ -78,6 +78,8 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView();
         User user = userService.getUserById(messageUser.getId());
         user.setRoles(roleService.findAllByRole(role));
+        user.setMessage(messageUser.getMessage());
+        user.setName(messageUser.getName());
         userService.updateUser(user);
         modelAndView.setViewName("redirect:/admin");
         return modelAndView;
